46521a2978e0ca4361320e89e396972be4945c26
Oppossome <dsecutler@gmail.com>
2025-05-21 12:50:20 -0400

    Commit Game.cs

---
+++ Code/Game.cs
@@ -0,0 +1,25 @@
+﻿using Sandbox.Network;
+
+namespace StylesheetReproduction;
+
+public class Game: Component, Component.INetworkListener, ISceneStartup
+{
+	[Property]
+	public GameObject PlayerPrefab { get; private set; }
+
+	void ISceneStartup.OnHostInitialize()
+	{
+		Networking.CreateLobby(new LobbyConfig()
+		{
+			DestroyWhenHostLeaves = true,
+			MaxPlayers = 2,
+		});
+	}
+
+	void INetworkListener.OnActive( Connection connection )
+	{
+		var player = PlayerPrefab.Clone();
+		player.Name = connection.DisplayName;
+		player.NetworkSpawn(connection);
+	}
+}
